dbcb6d553840c20bd10703285a37a2ec3148f351
Patrick Möller <patrick_moeller@live.de>
2021-10-18 12:11:17 +0200

    Renewing order structures

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { getGreeting } from './getGreeting.js'
 import { replaceName } from './replaceName.js'
 
-console.log(getGreeting('Jane Doe'))
+console.log(getGreeting('Michaelangelo'))
 console.log(replaceName('Hello Jane', 'Jane', 'John'))
 console.log(replaceName('Hello Jane. Goodbye Jane.', 'Jane', 'John'))
