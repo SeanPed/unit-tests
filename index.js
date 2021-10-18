5eb2b3a98e74510c84668d1ce06328df552d4f78
Patrick Möller <patrick_moeller@live.de>
2021-10-18 11:42:25 +0200

    Adding new functions

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-console.log('hello node')
-
 function getGreeting(name = 'you') {
-  return 'Hello' + name + '!'
+  return `Hello ${name}!`
 }
+console.log(getGreeting('Jane Doe'))
+
+function replaceName(text, name, newName) { 
+  return text.replaceAll(name, newName)
+}
+
+console.log(replaceName('Hello Jane', 'Jane', 'John'))
+console.log(replaceName('Hello Jane. Goodbye Jane.', 'Jane', 'John'))
+
